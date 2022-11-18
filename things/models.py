dd06d645f6eaa61989c46211c197ec015b5d7ccc
sdharren <sdharren@yahoo.co.uk>
2022-11-18 20:26:38 +0000

    changed desc to charfield

--- things/models.py
+++ things/models.py
@@ -4,5 +4,5 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 # Create your models here.
 class Thing(models.Model):
     name = models.CharField(max_length=30, unique=True)
-    description = models.TextField(blank=True, max_length=120)
+    description = models.CharField(blank=True, max_length=120)
     quantity = models.PositiveIntegerField(validators=[MinValueValidator(0), MaxValueValidator(100)])
